bdaf473e0a397e0dfeefa955d8acd1019be363a3
Admin <mangeshfulbone00@gmail.com>
2025-11-24 17:39:33 +0530

    YES

---
+++ Basic_Python/Dictionary_Iteration.py
@@ -0,0 +1,5 @@
+
+dict={1:'Master Card',2:'Visa',3:'Barkleys',4:'HSBC',5:'Google',6:'Microsoft',7:'Salesforce'}
+
+for i,value in dict.items():
+	print(i,value)
---
+++ Basic_Python/List-Slicing.py
@@ -0,0 +1,11 @@
+
+list=[10,20,30,40,50,60,70,"Ganesh","ram","Sita"]
+
+print(len(list))
+
+print(list[2:6])
+print(list[::-1])
+
+print(list[::])
+print(list[::-2])   
+print(list[-2])
