f4be33dcdafa7d33026f73a07852d8a542b023ca
Admin <mangeshfulbone00@gmail.com>
2026-02-16 15:03:36 +0530

    Pattern

---
+++ Pattern/Square.py
@@ -0,0 +1,6 @@
+n=int(input("Enter The Number of Rows:"))
+#print square pattern using nested loop 
+for i in range(n+1):
+    for j in range(n):
+        print("*",end=" ")
+    print()
---
+++ Pattern/om.py
@@ -0,0 +1,7 @@
+str="                                                                                                                                                                                                                                                          OM NAM SHIVAY:" , "HAR HAR MAHADEV:" , "OM NAMAH SHIVAY:" , "OM SHIVAYE NAMAHA:" , "OM NAMAH SHIVAYE:" , "OM NAMAH SHIVAYA:" , "OM NAMAH SHIVAY:" , "OM NAMAH SHIVA:" , "OM NAMAH SHIV:" , "OM NAMAH SHI:" , "OM NAMAH SH:" , "OM NAMAH S:" , "OM NAMAH :" , "OM NAMAH"
+for i in range(0,101):
+    print(i ,str)
+
+#print string  in marathi
+marathi_str = "ॐ नमः शिवाय"
+print(marathi_str)
