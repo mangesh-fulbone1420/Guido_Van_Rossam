30b8e7724365a034176b4408061fed13514483c3
Admin <mangeshfulbone00@gmail.com>
2026-04-11 11:12:21 +0530

    list

---
+++ Tuple/Tuple_Slicing.py
@@ -0,0 +1,10 @@
+# Python tuple are immutable sequence used to store a collection of heterogeneous data.
+# They are similar to list but cant be changed after created.
+
+# tuple slicing is a technique to extract a sub part of a tuple by spaecifing a start index and end index .
+tuple=(1,2,3,4,5,6,7)
+
+print("Original tuple is:",tuple)
+
+print("Revese tuple is :",tuple[::-1])
+print("Slicing Tuple is :",tuple[1:7:2])
